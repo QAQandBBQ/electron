1c84a0caa6e101a0bdce9275a51e735a0f9b477e
何勇 <heyong02530@51shebao.com>
2023-05-10 22:47:01 +0800

    feat(core): 计算器

--- src/main/index.js
+++ src/main/index.js
@@ -2,14 +2,21 @@ const { app, BrowserWindow, ipcMain, Menu, Tray } = require("electron");
 const path = require("path");
 const { tpl, ownTpl, trayMenu } = require("../config/menu.js");
 const fs = require("fs");
+const width = 300;
+const height = 420;
 
 let win = null;
 
 function createWindow(params) {
   win = new BrowserWindow({
-    width: 600,
-    height: 500,
-    // frame: false,
+    width,
+    height,
+    frame: false,
+    resizable: false,
+    show: false, // 注意这里 BrowserWindow 默认设置为不显示（show: false），只有当用户点击托盘图标的时候，才展示这个窗口。
+    movable: false,
+    minimizable: false,
+    maximizable: false,
     webPreferences: {
       sandbox: false,
       preload: path.join(__dirname, "../preload/index.js"),
@@ -17,6 +24,8 @@ function createWindow(params) {
   });
 
   win.loadFile(path.join(__dirname, "../renderer/index.html"));
+
+  win.setVisibleOnAllWorkspaces(true);
 
   win.webContents.openDevTools();
   let newTpl = [...tpl];
@@ -29,11 +38,38 @@ function createWindow(params) {
 
 app.whenReady().then(() => {
   createWindow();
-  onTray();
+  trayCounter();
   // createDockMenu();
 
   // installMenuPopup();
 });
+
+/**
+ * 托盘-计算器
+ */
+function trayCounter() {
+  const tray = new Tray(path.join(__dirname, "icon.png"));
+  // const posi = tray.getBounds();
+  // console.log(posi); // { x: 0, y: 900, width: 32, height: 24 }
+
+  tray.on("click", () => {
+    const trayBounds = tray.getBounds();
+    console.log(trayBounds, "trayBounds");
+    win.setPosition(
+      trayBounds.x + trayBounds.width / 2 - width / 2,
+      trayBounds.y + trayBounds.height / 2 - height / 2
+    );
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
+  });
+  // 当窗口失去焦点，自动隐藏
+  win.on("blur", () => {
+    win.hide();
+  });
+}
 
 function _popupMenu() {
   const menu = Menu.buildFromTemplate(tpl);
